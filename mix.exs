4921a53362b147206e35fe04083865f588739d5a
John Oxford <joxford531@gmail.com>
2018-08-27 22:09:17 -0400

    Add Application behavour to project.

--- mix.exs
+++ mix.exs
@@ -14,7 +14,8 @@ defmodule TodoList.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger]
+      extra_applications: [:logger],
+      mod: {Todo.Application, []}
     ]
   end
 
@@ -22,7 +23,7 @@ defmodule TodoList.MixProject do
   defp deps do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},      
+      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"},
     ]
   end
 end
